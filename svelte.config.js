ab20e41e5fb707e63937af63dae826c3d978ed9d
Inm0rt4LKoff33 <fabimv23@gmail.com>
2025-01-24 01:45:11 -0600

    yaml svelte adapter

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,7 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      fallback: undefined,
-      strict: false,
-    }),
+    adapter: adapter(),
   },
   preprocess: vitePreprocess(),
 };
