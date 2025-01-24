c49e1b817bb721505db15aa0a8e780559e0cd06c
Inm0rt4LKoff33 <fabimv23@gmail.com>
2025-01-24 02:11:03 -0600

    something something

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,15 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: undefined,
+      strict: false,
+    }),
     paths: {
-      base: process.env.NODE_ENV === "production" ? "My-Professional-Portfolio" : "",
+      base:
+        process.env.NODE_ENV === "production"
+          ? "/My-Professional-Portfolio"
+          : "",
     },
   },
   preprocess: vitePreprocess(),
