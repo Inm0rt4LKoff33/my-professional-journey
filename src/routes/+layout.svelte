bd7566803e01453f2f2ed5b21ee56d32758c65f4
Inm0rt4LKoff33 <fabimv23@gmail.com>
2025-01-24 17:25:14 -0600

    deployoyoyoy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
   import Header from "../components/Header.svelte";
   import Footer from "../components/Footer.svelte";
 
-  export const prerenderer = true;
+  export const prerender = true;
 
   let y;
   let innerHeight = 0;
