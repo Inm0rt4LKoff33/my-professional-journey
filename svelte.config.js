d735be898c9ceb3b8d4631944165e75822b569b6
Inm0rt4LKoff33 <fabimv23@gmail.com>
2025-01-24 17:03:47 -0600

    rada rada

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,13 @@ const dev = process.argv.includes("dev");
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+    }),
     paths: {
       base: dev ? "" : process.env.BASE_PATH,
     },
